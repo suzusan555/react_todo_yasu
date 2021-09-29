63e5b62f522935563d6957a6c69887664f5ac333
suzusan555 <ryuusei.8hatake@gmail.com>
2021-09-30 00:18:01 +0900

    classコンポーネント first commit

--- src/List.js
+++ src/List.js
@@ -1,11 +1,15 @@
-export const List = (props) => {
-  const title = props.title;
-  return (
-    <div>
-      <h4>{ title }</h4>
-      <p>リストです</p>
-    </div>
-  )
+import React from "react";
+
+export class List extends React.Component {
+  render() {
+    const { title } = this.props;
+    return(
+      <div>
+        <h4>{ title }</h4>
+        <p>リストです</p>
+      </div>
+    )
+  }
 }
 
 export default List;
