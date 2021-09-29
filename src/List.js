f543d09c6813effef4525ac8d1ca56ec7b0d9d14
suzusan555 <ryuusei.8hatake@gmail.com>
2021-09-30 00:56:51 +0900

    分岐定義

--- src/List.js
+++ src/List.js
@@ -1,12 +1,25 @@
 import React from "react";
 
+const LANGUAGES = [
+  'java',
+  'C++',
+  'PHP',
+  'Go',
+  'Ruby'
+];
+
 export class List extends React.Component {
   render() {
     const { title } = this.props;
     return(
       <div>
-        <h4>{ title }</h4>
-        <p>リストです</p>
+        {
+          LANGUAGES.map((lang,index) => {
+            return (
+              <div key={index}>{lang}</div>
+            )
+          })
+        }
       </div>
     )
   }
